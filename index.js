97eb91e31fc8fef0f834330926250cba1a72c004
candescent-constraint-5633 <atyreefinch@gmail.com>
2019-07-02 23:55:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+const theBeatlesPlay = (musicians,instruments) => {
+  let itBe = [];
+  for (let i = 0; i < musicians.length; i++) {
+   itBe.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return itBe;
+}
+
+const johnLennonFacts = (facts) => {
+  let i = 0;
+  let excitedFacts = [];
+  while (i<facts.length) {
+    excitedFacts.push(`${facts[i]}!!!`);
+    i++
+  }
+  return excitedFacts;
+}
+
